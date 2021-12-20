282efe9d3399260f9ed13fb24d13ad59828d1cf3
Osakpolor Obaseki <obasekiosa@gmail.com>
2021-12-20 17:06:08 +0100

    Add Testclass for Tuple3 class

---
+++ src/test/java/io/github/obasekiosa/raytracer/basic/Tuple3Test.java
@@ -0,0 +1,12 @@
+package io.github.obasekiosa.raytracer.basic;
+
+import org.junit.Before;
+
+public class Tuple3Test {
+    public double EPISILON;
+
+    @Before
+    public void setUp() {
+        this.EPISILON = 1e-4;
+    }
+}
