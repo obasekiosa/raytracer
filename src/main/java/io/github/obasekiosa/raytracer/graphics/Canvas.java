a5425433dc055e0a733a53401fd545ce7d1c6f03
Osakpolor Obaseki <obasekiosa@gmail.com>
2021-12-28 20:43:54 +0100

    Implement Canvas Class

--- src/main/java/io/github/obasekiosa/raytracer/graphics/Canvas.java
+++ src/main/java/io/github/obasekiosa/raytracer/graphics/Canvas.java
@@ -1,44 +1,77 @@
 package io.github.obasekiosa.raytracer.graphics;
 
-import io.github.obasekiosa.raytracer.exceptions.UnImplementedMethodException;
+import java.util.Arrays;
 
 public class Canvas {
-
-    private int width;
-    private int height;
 
     private final Color[][] canvas;
 
     public Canvas(int width, int height) {
-        throw new UnImplementedMethodException();
+        if (width <= 0 || height <= 0) {
+            throw new IllegalArgumentException("Width and Height must be > 0");
+        }
+        this.canvas = new Color[height][width];
+
+        for(int i = 0; i < this.canvas.length; i++)
+            for(int j = 0; j < this.canvas[0].length; j++)
+                this.canvas[i][j] = new Color(0, 0, 0);
     }
 
     public int getWidth() {
-        return width;
+        return this.canvas[0].length;
     }
 
-    public void setWidth(int width) {
-        this.width = width;
-    }
 
     public int getHeight() {
-        return height;
-    }
-
-    public void setHeight(int height) {
-        this.height = height;
+        return this.canvas.length;
     }
 
     public Color pixelAt(int x, int y) {
-        throw new UnImplementedMethodException();
+        if (x < 0 || x >= this.canvas[0].length || y < 0 || y >= this.canvas.length) {
+            throw new IllegalArgumentException("x and y must be between the boundries of the canvas");
+        }
+
+        Color pixel = this.canvas[y][x];
+        return new Color(pixel.getRed(), pixel.getGreen(), pixel.getBlue(), pixel.getAlpha());
     }
 
     public void writePixel(int x, int y, Color c) {
-        throw new UnImplementedMethodException();
+        if (x < 0 || x >= this.canvas[0].length || y < 0 || y >= this.canvas.length) {
+            throw new IllegalArgumentException("x and y must be between the boundries of the canvas");
+        }
+
+        this.canvas[y][x] = c;
     }
 
     public Color[][] getPixels() {
-        throw new UnImplementedMethodException();
+        Color[][] result = new Color[this.canvas.length][this.canvas[0].length];
+
+        for (int i = 0; i < result.length; i++)
+            for(int j = 0; j < result[0].length; j++)
+                result[i][j] = this.pixelAt(j, i);
+            
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Canvas [canvas= \n");
+
+        for (int i = 0; i < this.canvas.length; i++) {
+            for (int j = 0; j < this.canvas[0].length; j++) {
+                sb.append("(x=")
+                    .append(i)
+                    .append(", y=")
+                    .append(j)
+                    .append(") -> ")
+                    .append(this.canvas[i][j])
+                    .append("\t");
+            }
+            sb.append("\n");
+        }
+        sb.append("]");
+        return sb.toString();
     }
     
 }
