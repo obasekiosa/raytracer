64e0253ed8a5d537117d166306b240fd82ac3f9a
Osakpolor Obaseki <obasekiosa@gmail.com>
2021-12-28 19:26:19 +0100

    Add stubs for Canvas class

---
+++ src/main/java/io/github/obasekiosa/raytracer/graphics/Canvas.java
@@ -0,0 +1,11 @@
+package io.github.obasekiosa.raytracer.graphics;
+
+import io.github.obasekiosa.raytracer.exceptions.UnImplementedMethodException;
+
+public class Canvas {
+
+    public Canvas(int width, int height) {
+        throw new UnImplementedMethodException();
+    }
+    
+}
