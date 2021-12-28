ec2cd89088eb0537ec8fd04f6f75d5c270d0e5e5
Osakpolor Obaseki <obasekiosa@gmail.com>
2021-12-29 00:48:50 +0100

    Implement Canvas toPPM method

--- src/main/java/io/github/obasekiosa/raytracer/graphics/Canvas.java
+++ src/main/java/io/github/obasekiosa/raytracer/graphics/Canvas.java
@@ -1,10 +1,19 @@
 package io.github.obasekiosa.raytracer.graphics;
 
-import java.util.Arrays;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Paths;
+
+import io.github.obasekiosa.raytracer.exceptions.UnImplementedMethodException;
 
 public class Canvas {
 
     private final Color[][] canvas;
+
+    private final int LINE_MAX_LENGTH = 70;
 
     public Canvas(int width, int height) {
         if (width <= 0 || height <= 0) {
@@ -51,6 +60,103 @@ public class Canvas {
                 result[i][j] = this.pixelAt(j, i);
             
         return result;
+    }
+
+
+    public void toPPM() {
+        toPPM(null);
+    }
+
+
+    public void toPPM(String name) {
+
+        if (name == null) {
+            name = "output";
+        }
+        
+        File file = null;
+
+        try {
+            file = new File(name + ".ppm");
+            file.createNewFile();
+            boolean fileExists = file.exists();
+
+            if (!fileExists) {
+                throw new IOException("File could not be created");
+            }
+
+            StringBuilder sBuilder = new StringBuilder();
+
+            // write header
+            sBuilder.append("P3");
+            sBuilder.append(System.lineSeparator());
+            sBuilder.append(this.getWidth() + " " + this.getHeight());
+            sBuilder.append(System.lineSeparator());
+            sBuilder.append("255");
+            sBuilder.append(System.lineSeparator());
+
+            // write content
+            for (int i = 0; i < this.canvas.length; i++) {
+
+                StringBuilder line = new StringBuilder();
+                for (int j = 0; j < this.canvas[0].length; j++) {
+                    Color pixel = this.canvas[i][j];
+                    int r = Math.min(255, Math.max(0, (int) Math.ceil(pixel.getRed() * 255)));
+                    int g = Math.min(255, Math.max(0, (int) Math.ceil(pixel.getGreen() * 255)));
+                    int b = Math.min(255, Math.max(0, (int) Math.ceil(pixel.getBlue() * 255)));
+
+                    line = addChannel(sBuilder, line, r);
+                    line.append(" ");
+                    line = addChannel(sBuilder, line, g);
+                    line.append(" ");
+                    line = addChannel(sBuilder, line, b);
+                    if (j != this.canvas[0].length - 1) {
+                        line.append(" ");
+                    }
+                        
+                }
+
+                if (line.length() > 0) {
+                    sBuilder.append(line.toString());
+                }
+                sBuilder.append(System.lineSeparator());
+            }
+
+            BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+            bw.append(sBuilder.toString());
+            bw.close();
+
+        } catch (IOException e) {
+            if (file != null) {
+                file.delete();
+            }
+            e.printStackTrace();
+            System.out.println("Error: Could not create file");
+        }
+    }
+
+    /**
+     * Adds the string representation of value to line and returns line if the total length of line would be 
+     * <= LINE_MAX_LENGTH otherwise it removes the last charater of line if it is a white space, creates a 
+     * new line and adds the string representation of value to the new StringBuilder and returns it
+     * @param sBuilder StringBuilder
+     * @param line StringBuilder
+     * @param value int
+     * @return StringBuilder
+     */
+    private StringBuilder addChannel(StringBuilder sBuilder, StringBuilder line, int value) {
+        String valueStr = String.valueOf(value);
+        if ((line.length() + valueStr.length()) > LINE_MAX_LENGTH) {
+
+            line.deleteCharAt(line.length() - 1);
+            line.append(System.lineSeparator());
+
+            sBuilder.append(line.toString());
+            line = new StringBuilder();
+
+        }
+        line.append(valueStr);
+        return line;
     }
 
     @Override
