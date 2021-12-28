d04f97eb62869c7acf504ed95ea7c839d39b4ad1
Osakpolor Obaseki <obasekiosa@gmail.com>
2021-12-28 20:03:25 +0100

    Add tests for canvas creation and writing

--- src/test/java/io/github/obasekiosa/raytracer/graphics/CanvasTest.java
+++ src/test/java/io/github/obasekiosa/raytracer/graphics/CanvasTest.java
@@ -1,6 +1,7 @@
 package io.github.obasekiosa.raytracer.graphics;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -11,8 +12,25 @@ public class CanvasTest {
     public void test_InstanceMethod_Constructor_ReturnCanvas() {
         Canvas canvas = new Canvas(10, 20);
 
+        assertTrue("Is instance of Canvas", canvas instanceof Canvas);
         assertEquals("Width of Canvas is ", 10, canvas.getWidth());
         assertEquals("Height of Canvas is ", 20, canvas.getHeight());
+
+        Color zero = new Color(0, 0, 0);
+        for (Color[] row : canvas.getPixels())
+            for (Color c : row) assertTrue("Pixel is Zero", c.equals(zero));
     }
+
+    @Test
+    public void test_Behaviour_Write_And_Read_Pixel() {
+        Canvas canvas = new Canvas(10, 20);
+        Color red = new Color(1, 0, 0);
+
+        canvas.writePixel(2, 3, red);
+        boolean result =  red.equals(canvas.pixelAt(2, 3));
+        assertEquals("Pixel was written onto the canvas", result);
+    }
+
+
     
 }
