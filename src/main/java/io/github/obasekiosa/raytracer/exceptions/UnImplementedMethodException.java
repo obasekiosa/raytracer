378a892886f8c6ad289ce79611d1326ce4e4308b
Osakpolor Obaseki <obasekiosa@gmail.com>
2021-12-28 19:26:05 +0100

    Implement UnImplementedMethodException class

---
+++ src/main/java/io/github/obasekiosa/raytracer/exceptions/UnImplementedMethodException.java
@@ -0,0 +1,9 @@
+package io.github.obasekiosa.raytracer.exceptions;
+
+public class UnImplementedMethodException extends UnsupportedOperationException {
+
+    public UnImplementedMethodException() {
+        super("This method has not yet Been Implemented");
+    }
+    
+}
