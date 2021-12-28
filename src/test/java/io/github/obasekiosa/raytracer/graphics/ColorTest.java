f78678c9b60d61e9758e6ae995c1dc4dde80e5b6
Osakpolor Obaseki <obasekiosa@gmail.com>
2021-12-28 19:17:33 +0100

    Add Color tests for add, subtract, multiply methods

--- src/test/java/io/github/obasekiosa/raytracer/graphics/ColorTest.java
+++ src/test/java/io/github/obasekiosa/raytracer/graphics/ColorTest.java
@@ -17,15 +17,51 @@ public class ColorTest {
 
     @Test
     public void test_InstanceMethod_3ArgConstructor_CreatesAndReturnsColor() {
-        Color color = new Color(0.5, 0.4, 1.7);
+        Color color = new Color(-0.5, 0.4, 1.7);
 
-        verifyColor(0.5, 0.4, 1.7, 1.0, color, this.EPISILON);
+        verifyColor(-0.5, 0.4, 1.7, 1.0, color, this.EPISILON);
     }
 
     @Test
     public void test_InstanceMethod_4ArgConstructor_CreatesAndReturnsColor() {
-        Color color = new Color(0.5, 0.4, 1.7, 0.3);
+        Color color = new Color(-0.5, 0.4, 1.7, 0.3);
 
-        verifyColor(0.5, 0.4, 1.7, 0.3, color, this.EPISILON);
+        verifyColor(-0.5, 0.4, 1.7, 0.3, color, this.EPISILON);
+    }
+
+    @Test
+    public void test_InstanceMethod_Add_ColorReturnColor() {
+        Color color = new Color(0.9, 0.6, 0.75);
+        Color otherColor = new Color(0.7, 0.1, 0.25);
+
+        Color result = color.add(otherColor);
+        verifyColor(1.6, 0.7, 1.0, 1.0, result, this.EPISILON);
+    }
+
+    @Test
+    public void test_InstanceMethod_Subtract_ColorReturnColor() {
+        Color color = new Color(0.9, 0.6, 0.75);
+        Color otherColor = new Color(0.7, 0.1, 0.25);
+
+        Color result = color.subtract(otherColor);
+        verifyColor(0.2, 0.5, 0.5, 1.0, result, this.EPISILON);
+    }
+
+    @Test
+    public void test_InstanceMethod_Multiply_DoubleReturnColor() {
+        Color color = new Color(0.2, 0.3, 0.4);
+        double scalar = 2;
+
+        Color result = color.multiply(scalar);
+        verifyColor(0.4, 0.6, 0.8, 1.0, result, this.EPISILON);
+    }
+
+    @Test
+    public void test_InstanceMethod_Multiply_ColorReturnColor() {
+        Color color = new Color(1, 0.2, 0.4);
+        Color otherColor = new Color(0.9, 1, 0.1);
+
+        Color result = color.multiply(otherColor);
+        verifyColor(0.9, 0.2, 0.04, 1.0, result, this.EPISILON);
     }
 }
