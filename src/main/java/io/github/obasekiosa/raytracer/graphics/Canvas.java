034f7e62f731ce90191217f80a69d9b18b21d41f
Osakpolor Obaseki <obasekiosa@gmail.com>
2021-12-28 19:32:21 +0100

    Update Canvas stub

--- src/main/java/io/github/obasekiosa/raytracer/graphics/Canvas.java
+++ src/main/java/io/github/obasekiosa/raytracer/graphics/Canvas.java
@@ -4,7 +4,34 @@ import io.github.obasekiosa.raytracer.exceptions.UnImplementedMethodException;
 
 public class Canvas {
 
+    private int width;
+    private int height;
+
     public Canvas(int width, int height) {
+        throw new UnImplementedMethodException();
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public Color pixelAt(int x, int y) {
+        throw new UnImplementedMethodException();
+    }
+
+    public void writePixel(int x, int y, Color c) {
         throw new UnImplementedMethodException();
     }
     
