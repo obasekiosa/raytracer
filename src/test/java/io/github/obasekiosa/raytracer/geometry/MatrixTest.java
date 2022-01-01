ddf48b2b2ade8b0b95b83f39da4591eed14258af
Osakpolor Obaseki <obasekiosa@gmail.com>
2022-01-01 15:00:01 +0100

    Add Matrix tests

--- src/test/java/io/github/obasekiosa/raytracer/geometry/MatrixTest.java
+++ src/test/java/io/github/obasekiosa/raytracer/geometry/MatrixTest.java
@@ -39,6 +39,20 @@ public class MatrixTest {
 
         verifyMatrix(values, 4, 4, matrix, this.EPISILON);
     }
+
+    @Test
+    public void test_StaticMethod_From_ArrayOfDoublesCreateMatrix() {
+        double[][] values = {
+                { 1, 2, 3, 4 },
+                { 5.5, 6.5, 7.5, 8.5 },
+                { 9, 10, 11, 12 },
+                { 13.5, 14.5, 15.5, 16.5 },
+        };
+
+        Matrix matrix = Matrix.from(values);
+    
+        verifyMatrix(values, 4, 4, matrix, this.EPISILON);
+    }
     
     @Test
     public void test_InstanceMethod_Constructor_OneIntegerCreate4x4Matrix() {
@@ -385,7 +399,7 @@ public class MatrixTest {
 
 
     @Test
-    public void test_InstanceMethod_SubMatrix_3x3Returns2x2Matrix() {
+    public void test_InstanceMethod_SubMatrix_3x3Return2x2Matrix() {
         double[][] values = {
             {1, 5, 0},
             {-3, 2, 7},
@@ -409,7 +423,7 @@ public class MatrixTest {
     }
     
     @Test
-    public void test_InstanceMethod_SubMatrix_4x4Returns3x3Matrix() {
+    public void test_InstanceMethod_SubMatrix_4x4Return3x3Matrix() {
         double[][] values = {
             {-6, 1, 1, 6},
             {-8, 5, 8, 6},
@@ -432,6 +446,243 @@ public class MatrixTest {
         Matrix result = matrix.subMatrix(2, 1);
 
         verifyMatrix(expectedValues, 3, 3, result, this.EPISILON);
+    }
+
+    @Test
+    public void test_InstanceMethod_Minor_ReturnDouble() {
+        double[][] values = {
+            {3, 5, 0},
+            {2, -1, -7},
+            {6, -1, 5},
+        };
+
+        Matrix matrix = Matrix.from(values);
+        Matrix B = matrix.subMatrix(1, 0);
+
+        double determinant = B.determinant();
+        assertEquals("Determinant of submatrix", 25, determinant, this.EPISILON);
+
+        double minor = matrix.minor(1, 0);
+        assertEquals("Minor of matrix", 25, minor, this.EPISILON);
+
+    }
+
+    @Test
+    public void test_InstanceMethod_Cofactor_ReturnDouble() {
+        double[][] values = {
+                { 3, 5, 0 },
+                { 2, -1, -7 },
+                { 6, -1, 5 },
+        };
+
+        Matrix A = Matrix.from(values);
+
+        double minor00 = A.minor(0, 0);
+        assertEquals("Computed minor of matrix", -12, minor00, this.EPISILON);
+        
+        double cofactor00 = A.cofactor(0, 0);
+        assertEquals("Computed cofactor of matrix", -12, cofactor00, this.EPISILON);
+
+        double minor10 = A.minor(1, 0);
+        assertEquals("Computed minor of matrix", 25, minor10, this.EPISILON);
+
+        double cofactor10 = A.cofactor(1, 0);
+        assertEquals("Computed cofactor of matrix", -25, cofactor10, this.EPISILON);
+    }
+    
+    @Test
+    public void test_InstanceMethod_Determinant_3x3MatrixReturnDouble() {
+        double[][] values = {
+                { 1, 2, 6 },
+                { -5, 8, -4 },
+                { 2, 6, 4 },
+        };
+
+        Matrix A = Matrix.from(values);
+        double cofactor00 = A.cofactor(0, 0);
+        assertEquals("Cofactor of matrix computed", 56, cofactor00, this.EPISILON);
+
+        double cofactor01 = A.cofactor(0, 1);
+        assertEquals("Cofactor of matrix computed", 12, cofactor01, this.EPISILON);
+
+        double cofactor02 = A.cofactor(0, 2);
+        assertEquals("Cofactor of matrix computed", -46, cofactor02, this.EPISILON);
+
+        double determinant = A.determinant();
+        assertEquals("Determinant of matrix computed", -196, determinant, this.EPISILON);
+    }
+
+    @Test
+    public void test_InstanceMethod_Determinant_4x4MatrixReturnDouble() {
+        double[][] values = {
+                { -2, -8, 3, 5 },
+                { -3, 1, 7, 3 },
+                { 1, 2, -9, 6 },
+                { -6, 7, 7, -9 }
+        };
+
+        Matrix A = Matrix.from(values);
+        double cofactor00 = A.cofactor(0, 0);
+        assertEquals("Cofactor of matrix computed", 690, cofactor00, this.EPISILON);
+
+        double cofactor01 = A.cofactor(0, 1);
+        assertEquals("Cofactor of matrix computed", 447, cofactor01, this.EPISILON);
+
+        double cofactor02 = A.cofactor(0, 2);
+        assertEquals("Cofactor of matrix computed", 210, cofactor02, this.EPISILON);
+
+        double cofactor03 = A.cofactor(0, 3);
+        assertEquals("Cofactor of matrix computed", 51, cofactor03, this.EPISILON);
+
+        double determinant = A.determinant();
+        assertEquals("Determinant of matrix computed", -4071, determinant, this.EPISILON);
+    }
+
+    @Test
+    public void test_InstanceMethod_Invertible_ReturnBooleanTrue() {
+        double[][] values = {
+            {6, 4, 4, 4},
+            {5, 5, 7, 6},
+            {4, -9, 3, -7},
+            {9, 1, 7, -6}
+        };
+
+        Matrix A = Matrix.from(values);
+        double determinant = A.determinant();
+        assertEquals("Computed determinat of matrix", -2120, determinant, this.EPISILON);
+
+        boolean isInvertible = A.isInvertible();
+        assertTrue("Matrix is Invertible", isInvertible);
+    }
+    
+    @Test
+    public void test_InstanceMethod_Invertible_ReturnBooleanFalse() {
+        double[][] values = {
+            {-4, 2, -2, -3},
+            {9, 6, 2, 6},
+            {0, -5, 1, -5},
+            {0, 0, 0, 0}
+        };
+
+        Matrix A = Matrix.from(values);
+        double determinant = A.determinant();
+        assertEquals("Computed determinat of matrix", 0, determinant, this.EPISILON);
+
+        boolean isInvertible = A.isInvertible();
+        assertFalse("Matrix is not Invertible", isInvertible);
+    }
+    
+
+    @Test
+    public void test_InstanceMethod_Inverse_ShouldRetrunTheInverseOf4x4MatrixWithCorrectIntermediateSteps() {
+        double[][] values = {
+            {-5, 2, 6, -8},
+            {1, -5, 1, 8},
+            {7, 7, -7, -7},
+            {1, -3, 7, 4}
+        };
+
+        Matrix A = Matrix.from(values);
+
+        Matrix B = A.inverse();
+
+        double ADeterminant = A.determinant();
+        assertEquals("Determinat of matrix", 532, ADeterminant, this.EPISILON);
+
+        double ACofactor23 = A.cofactor(2, 3);
+        assertEquals("Cofactor 2 3 of matrix", -160, ACofactor23, this.EPISILON);
+
+        double B32 = B.getEntry(3, 2);
+        assertEquals("Inverse matrix entry at 3, 2", -0.30075, B32, this.EPISILON); // -160/532
+
+        double ACofactor32 = A.cofactor(3, 2);
+        assertEquals("Cofactor 2 3 of matrix", 105, ACofactor32, this.EPISILON);
+
+        double B23 = B.getEntry(2, 3);
+        assertEquals("Inverse matrix entry at 2, 3", 0.19737, B23, this.EPISILON); // 105/532
+
+        double[][] BValues = {
+            {0.21805, 0.45113, 0.24060, -0.04511},
+            {-0.80827, -1.45677, -0.44361, 0.52068},
+            {-0.07895, -0.22368, -0.05263, 0.19737},
+            {-0.52256, -0.81391, -0.30075, 0.30639}
+        };
+
+        verifyMatrix(BValues, 4, 4, B, this.EPISILON);
+
+    }
+    
+    @Test
+    public void test_InstanceMethod_Inverse_ShouldRetrunTheInverseOf4x4Matrix() {
+        double[][][][] values = {
+            {
+                {
+                    { 8, -5, 9, 2},
+                    { 7, 5, 6, 1},
+                    { -6, 0, 9, 6},
+                    { -3, 0, -9, -4}
+                },
+                {
+                    { -0.15385, -0.15385, -0.28205, -0.53846 },
+                    { -0.07692, 0.12308, 0.02564, 0.03077 },
+                    { 0.35897, 0.35897, 0.43590, 0.92308 },
+                    { -0.69231, -0.69231, -0.76923, -1.92308 }
+                }, 
+            },
+            {
+                {
+                    { 9, 3, 0, 9},
+                    { -5, -2, -6, -3},
+                    { -4, 9, 6, 4},
+                    { -7, 6, 6, 2}
+                },
+                {
+                    { -0.04074, -0.07778, 0.14444, -0.22222 },
+                    { -0.07778, 0.03333, 0.36667, -0.33333 },
+                    { -0.02901, -0.14630, -0.10926, 0.12963 },
+                    { 0.17778, 0.06667, -0.26667, 0.33333 }
+                }, 
+            },
+            
+        };
+
+        for (int i = 0; i < values.length; i++) {
+            double[][] AValues = values[i][0];
+            double[][] InverseValues = values[i][1];;
+
+            Matrix A = Matrix.from(AValues);
+            Matrix B = A.inverse();
+            verifyMatrix(InverseValues, 4, 4, B, this.EPISILON);
+        }
+        
+    }
+
+    @Test void test_InstanceMethod_Inverse_ShouldReverseProductByMultiplicaton() {
+        double[][] valuesA = {
+            {3, -9, 7, 3},
+            {3, -8, 2, -9},
+            {-4, 4, 4, 1},
+            {-6, 5, -1, 1}
+        };
+
+        double[][] valuesB = {
+            {8, 2, 2, 2},
+            {3, -1, 7, 0},
+            {7, 0, 5, 4},
+            {6, -2, 0, 5}
+        };
+
+        Matrix A = Matrix.from(valuesA);
+        Matrix B = Matrix.from(valuesB);
+
+        Matrix C = A.multiply(B); // A * B
+        Matrix inverseB = B.inverse(); // B'
+
+        Matrix D = C.multiply(inverseB); // A * B * B'
+
+        boolean dEqualsA = D.equals(A); // A * B * B' = A
+
+        assertTrue("Multiplying Product By inverse reverses product", dEqualsA);
     }
     
 }
